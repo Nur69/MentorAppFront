e0c377815136c0c208eb8621049dfd6e1a53b98c
Nour Bennour <nour.bennour@medtech.tn>
2020-04-23 10:37:31 +0100

    Mycommit

---
+++ src/app/sessions/sessions.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-sessions',
+  templateUrl: './sessions.component.html',
+  styleUrls: ['./sessions.component.css']
+})
+export class SessionsComponent implements OnInit {
+  
+
+  sessions;
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit() {
+    // this.apiService.getSessions(this.apiService.user.email).subscribe((data) => {
+    //   console.log(data);
+    //   this.sessions = data;
+    // });
+    this.apiService.listMentors('').subscribe((data: any) => {
+      console.log(data);
+      this.sessions = data.users.filter(u => (u.sessions || []).includes(this.apiService.user._id));
+    })
+  }
+  updateReview(_id, score,course,idBtn) {
+    console.log(document.getElementById(idBtn));
+    document.getElementById(idBtn).hidden = true;
+    this.apiService.updateReview(_id, score).subscribe((data) => {
+      console.log(data);
+      
+      
+
+    })
+  }
+}
